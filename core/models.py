79ec06e09f1630d3629481fc98c14b11df6147fe
Anh Le <anh.ht.le@gmail.com>
2026-05-08 20:05:00 -0700

    migrations

---
+++ core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from django.db import models 
+from django.contrib.auth import get_user_model
+
+User = get_user_model() 
+
+
+class Permission(models.Model): 
+    key = models.CharField(max_length=128, unique=True) 
+    name = models.CharField(max_length=255) 
+    description = models.TextField(blank=True)
+    
+    class Meta: 
+        ordering = ["key"] 
+        def __str__(self): 
+            return self.key 
+
+
+class Group(models.Model): 
+    key = models.CharField(max_length=128, unique=True) 
+    name = models.CharField(max_length=255) 
+    permissions = models.ManyToManyField(Permission, related_name="groups", blank=True) 
+    
+
+class UserAccess(models.Model): 
+    user = models.OneToOneField( User, on_delete=models.CASCADE, related_name="user_access") 
+    groups = models.ManyToManyField( Group, related_name="users", blank=True) 
+    permissions = models.ManyToManyField(Permission, related_name="users", blank=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    user_access = models.OneToOneField(UserAccess, on_delete=models.SET_NULL, null=True, blank=True, related_name="profile")
